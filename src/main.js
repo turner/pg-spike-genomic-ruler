134824b71447c16ccc5991db510fdf4e51cfced0
turner <douglass.turner@gmail.com>
2025-01-06 16:34:35 -0500

    Remove chatter from ruler dragging

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 import './style.scss';
-import { initRuler } from './components/ruler.js';
-import RulerClass from "./components/rulerClass.js"
+import Ruler from "./components/ruler.js"
 
 document.addEventListener("DOMContentLoaded", (event) => {
 
     const canvas = document.getElementById("ruler-canvas");
-    const renderedExtent =
+    const genomicState =
         {
             startBP: 30000000,
             endBP: 40000000
         }
 
-    const genomicRuler = new RulerClass(canvas, 0, 133324548, renderedExtent)
+    const genomicRuler = new Ruler(canvas, 0, 133324548, genomicState)
+
+    document.getElementById('switchChromosomeBtn').addEventListener('click', () => {
+        const newChrStartBP = 0;
+        const newChrEndBP = 5e7; // 50 million base pairs
+        const newGenomicState = { startBP: 2e6, endBP: 3e6 }; // New visible region (2M - 3M bp)
+
+        genomicRuler.setChromosome(newChrStartBP, newChrEndBP, newGenomicState);
+    });
+
 
 })
