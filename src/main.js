7b1281fc481fb06078986688618c6cdd144a46f6
turner <douglass.turner@gmail.com>
2025-04-09 09:53:15 -0400

    BEM now used for locusInput

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const genomicRuler = new Ruler(canvas);
 
-    const locusInput = new LocusInput(document.getElementById('locus-input-container'), genomicRuler);
+    const locusInput = new LocusInput(document.querySelector('.locus-input'), genomicRuler);
 
     genomicRuler.setGenomicLocus('chr7', 30000000, 40000000);
 });
